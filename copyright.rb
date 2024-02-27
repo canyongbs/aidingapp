d0f58906d172eb9dff3bf17c2957b9cb04230977
Kevin Ullyott <ullyott.kevin@gmail.com>
2024-02-27 16:11:18 -0500

    Update the copyright in the ruby script
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>
    
    # Conflicts:
    #	copyright.rb

--- copyright.rb
+++ copyright.rb
@@ -6,8 +6,8 @@ COPYRIGHT = <<EOM
 
     Copyright © 2016-#{YEAR}, Canyon GBS LLC. All rights reserved.
 
-    Advising App™ is licensed under the Elastic License 2.0. For more details,
-    see https://github.com/canyongbs/aidingapp/blob/main/LICENSE.
+    Aiding App™ is licensed under the Elastic License 2.0. For more details,
+    see <https://github.com/canyongbs/aidingapp/blob/main/LICENSE.>
 
     Notice:
 
@@ -21,7 +21,7 @@ COPYRIGHT = <<EOM
       of the licensor in the software. Any use of the licensor’s trademarks is subject
       to applicable law.
     - Canyon GBS LLC respects the intellectual property rights of others and expects the
-      same in return. Canyon GBS™ and Advising App™ are registered trademarks of
+      same in return. Canyon GBS™ and Aiding App™ are registered trademarks of
       Canyon GBS LLC, and we are committed to enforcing and protecting our trademarks
       vigorously.
     - The software solution, including services, infrastructure, and code, is offered as a
@@ -30,7 +30,7 @@ COPYRIGHT = <<EOM
       in the Elastic License 2.0.
 
     For more information or inquiries please visit our website at
-    https://www.canyongbs.com or contact us via email at legal@canyongbs.com.
+    <https://www.canyongbs.com> or contact us via email at legal@canyongbs.com.
 EOM
 
 def evaluateFile(filePath, startTag, endTag)
