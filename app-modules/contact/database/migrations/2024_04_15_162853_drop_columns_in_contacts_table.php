c099a88dc12321922edbb4c8eb402b8d7f5026a2
joelicatajr <joelicatajr@users.noreply.github.com>
2024-04-15 17:11:28 +0000

    chore: fix code style

--- app-modules/contact/database/migrations/2024_04_15_162853_drop_columns_in_contacts_table.php
+++ app-modules/contact/database/migrations/2024_04_15_162853_drop_columns_in_contacts_table.php
@@ -34,15 +34,14 @@
 </COPYRIGHT>
 */
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
-        if(Schema::hasTable('contacts')){
+        if (Schema::hasTable('contacts')) {
             Schema::table('contacts', function (Blueprint $table) {
                 $table->dropColumn(['birthdate', 'hsgrad', 'email_2']);
             });
@@ -51,8 +50,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::table('contacts', function (Blueprint $table) {
-            //
-        });
+        Schema::table('contacts', function (Blueprint $table) {});
     }
 };
