d70790cea53188137d270b8609ee1987211aab92
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-06-12 20:28:07 +0000

    Remove reference to class
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- database/migrations/2024_06_12_201706_data_fix_kb_article_media_collection_names.php
+++ database/migrations/2024_06_12_201706_data_fix_kb_article_media_collection_names.php
@@ -36,13 +36,12 @@
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
-use AidingApp\KnowledgeBase\Models\KnowledgeBaseItem;
 
 return new class () extends Migration {
     public function up(): void
     {
         DB::table('media')
-            ->where('model_type', (new KnowledgeBaseItem())->getMorphClass())
+            ->where('model_type', 'knowledge_base_item')
             ->where('collection_name', 'solution')
             ->update(['collection_name' => 'article_details']);
     }
@@ -50,7 +49,7 @@ return new class () extends Migration {
     public function down(): void
     {
         DB::table('media')
-            ->where('model_type', (new KnowledgeBaseItem())->getMorphClass())
+            ->where('model_type', 'knowledge_base_item')
             ->where('collection_name', 'article_details')
             ->update(['collection_name' => 'solution']);
     }
