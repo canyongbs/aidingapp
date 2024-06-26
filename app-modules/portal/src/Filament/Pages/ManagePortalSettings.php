20c5a5739bf6f62d3b50e89eacd13e8a9b737846
payal-canyon <payal.baldaniya@canyongbs.com>
2024-06-26 17:53:49 +0000

    [AIDAPP-150]: Setting a favicon or logo should not be possible when KB is disabled

--- app-modules/portal/src/Filament/Pages/ManagePortalSettings.php
+++ app-modules/portal/src/Filament/Pages/ManagePortalSettings.php
@@ -143,6 +143,7 @@ class ManagePortalSettings extends SettingsPage
                         SpatieMediaLibraryFileUpload::make('favicon')
                             ->collection('portal_favicon')
                             ->visibility('private')
+                            ->visible(Gate::check(Feature::KnowledgeManagement->getGateName()))
                             ->acceptedFileTypes([
                                 'image/png',
                                 'image/jpeg',
@@ -161,7 +162,7 @@ class ManagePortalSettings extends SettingsPage
                             ->model(
                                 SettingsProperty::getInstance('portal.logo'),
                             )
-                            ->visible(PennantFeature::active('portal-logo')),
+                            ->visible(Gate::check(Feature::KnowledgeManagement->getGateName()) && PennantFeature::active('portal-logo')),
                         Actions::make([
                             Action::make('view')
                                 ->url(fn () => route('portal.knowledge-management.show'))
