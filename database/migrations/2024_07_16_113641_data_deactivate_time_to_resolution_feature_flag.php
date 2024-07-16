45923da939fcc93b3ee8a802e85fc3ed865168a9
joelicatajr <joelicatajr@users.noreply.github.com>
2024-07-16 12:01:38 +0000

    chore: fix code style

--- database/migrations/2024_07_16_113641_data_deactivate_time_to_resolution_feature_flag.php
+++ database/migrations/2024_07_16_113641_data_deactivate_time_to_resolution_feature_flag.php
@@ -37,8 +37,7 @@
 use Laravel\Pennant\Feature;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Feature::purge('time_to_resolution');
