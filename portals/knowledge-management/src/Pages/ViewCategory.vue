cc3df5ddf3b7c72acf25049d9045cd2ea32ad272
payal-canyon <payal.baldaniya@canyongbs.com>
2024-07-29 18:58:30 +0000

    [AIDAPP-202]: Fixed the display of the search result, UI fixes

--- portals/knowledge-management/src/Pages/ViewCategory.vue
+++ portals/knowledge-management/src/Pages/ViewCategory.vue
@@ -32,6 +32,7 @@
 </COPYRIGHT>
 -->
 <script setup>
+    import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
     import { defineProps, ref, watch, onMounted } from 'vue';
     import { useRoute } from 'vue-router';
     import Breadcrumbs from '../Components/Breadcrumbs.vue';
@@ -41,8 +42,8 @@
     import { ChevronRightIcon, XMarkIcon } from '@heroicons/vue/20/solid/index.js';
     import Tags from '../Components/Tags.vue';
     import Article from '../Components/Article.vue';
-    import { useAuthStore } from '../Stores/auth.js';
-    import { useFeatureStore } from '../Stores/feature.js';
+    import SearchResults from '../Components/SearchResults.vue';
+    import Badge from '../Components/Badge.vue';
 
     const route = useRoute();
 
@@ -66,6 +67,7 @@
     });
 
     const loadingResults = ref(true);
+    const loadingeSearchResults = ref(true);
     const category = ref(null);
     const articles = ref(null);
     const searchQuery = ref('');
@@ -81,19 +83,16 @@
             return;
         }
 
-        loadingResults.value = true;
+        loadingeSearchResults.value = true;
 
         post(props.searchUrl, {
             search: JSON.stringify(value),
             tags: selectedTags.value.join(','),
         }).then((response) => {
             searchResults.value = response.data;
-            loadingResults.value = false;
+            loadingeSearchResults.value = false;
         });
     }, 500);
-
-    const { user } = useAuthStore();
-    const { hasServiceManagement } = useFeatureStore();
 
     watch(searchQuery, (value) => {
         debounceSearch(value);
@@ -151,6 +150,52 @@
 </script>
 
 <template>
+    <div class="top-0 z-40 flex flex-col items-center bg-gray-50">
+        <div class="bg-gradient-to-br from-primary-500 to-primary-800 w-full px-6">
+            <div class="max-w-screen-xl flex flex-col gap-y-6 mx-auto py-8">
+                <div class="flex flex-col gap-y-1 text-left">
+                    <h3 class="text-3xl font-semibold text-white">Need help?</h3>
+                    <p class="text-primary-100">Search our knowledge base for advice and answers</p>
+                </div>
+
+                <form action="#" method="GET">
+                    <label for="search" class="sr-only">Search</label>
+                    <div class="relative rounded">
+                        <div>
+                            <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 py-3">
+                                <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </div>
+                            <input
+                                type="search"
+                                v-model="searchQuery"
+                                id="search"
+                                placeholder="Search for articles and categories"
+                                class="block w-full rounded border-0 pl-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-2-- sm:text-sm sm:leading-6"
+                                :class="{ 'rounded-b-none': tags.length > 0 }"
+                            />
+                        </div>
+                    </div>
+                    <details
+                        v-if="tags.length > 0"
+                        class="rounded rounded-t-none bg-white py-3 p-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-2-- sm:text-sm sm:leading-6"
+                    >
+                        <summary v-if="selectedTags.length > 0">Tags ({{ selectedTags.length }} selected)</summary>
+                        <summary v-else>Tags</summary>
+                        <div class="flex flex-wrap gap-2">
+                            <Badge
+                                v-for="tag in tags"
+                                :key="tag.id"
+                                :value="tag.name"
+                                class="cursor-pointer"
+                                :class="{ 'bg-primary-600 text-white': selectedTags.includes(tag.id) }"
+                                @click="toggleTag(tag.id)"
+                            />
+                        </div>
+                    </details>
+                </form>
+            </div>
+        </div>
+    </div>
     <div class="sticky top-0 z-40 flex flex-col items-center bg-gray-50">
         <div class="w-full px-6">
             <div class="max-w-screen-xl flex flex-col gap-y-6 mx-auto py-8">
@@ -158,84 +203,44 @@
                     <AppLoading />
                 </div>
                 <div v-else>
-                    <div class="flex flex-col gap-y-1 text-left">
-                        <h3 class="text-3xl font-semibold text-white">Need help?</h3>
-                        <p class="text-primary-100">Search our knowledge base for advice and answers</p>
-                    </div>
-
-                    <form action="#" method="GET">
-                        <label for="search" class="sr-only">Search</label>
-                        <div class="relative rounded">
-                            <div>
-                                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 py-3">
-                                    <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </div>
-                                <input
-                                    type="search"
-                                    v-model="searchQuery"
-                                    id="search"
-                                    placeholder="Search for articles and categories"
-                                    class="block w-full rounded border-0 pl-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-2-- sm:text-sm sm:leading-6"
-                                    :class="{ 'rounded-b-none': tags.length > 0 }"
-                                />
-                            </div>
-                        </div>
-                        <details
-                            v-if="tags.length > 0"
-                            class="rounded rounded-t-none bg-white py-3 p-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-2-- sm:text-sm sm:leading-6"
-                        >
-                            <summary v-if="selectedTags.length > 0">Tags ({{ selectedTags.length }} selected)</summary>
-                            <summary v-else>Tags</summary>
-                            <div class="flex flex-wrap gap-2">
-                                <Badge
-                                    v-for="tag in tags"
-                                    :key="tag.id"
-                                    :value="tag.name"
-                                    class="cursor-pointer"
-                                    :class="{ 'bg-primary-600 text-white': selectedTags.includes(tag.id) }"
-                                    @click="toggleTag(tag.id)"
-                                />
-                            </div>
-                        </details>
-                    </form>
-
                     <main class="flex flex-col gap-8">
-                        <Breadcrumbs currentCrumb="Categories"></Breadcrumbs>
-
-                        <!-- <div class="max-w-screen-xl flex flex-col gap-y-6 mx-auto py-8"> -->
+                        <Breadcrumbs :currentCrumb="category.name"></Breadcrumbs>
+                        <div v-if="searchQuery || selectedTags.length > 0" class="flex flex-col gap-6">
                             <SearchResults
-                                v-if="searchQuery || selectedTags.length > 0"
                                 :searchQuery="searchQuery"
                                 :searchResults="searchResults"
-                                :loadingResults="loadingResults"
+                                :loadingResults="loadingeSearchResults"
                             >
                             </SearchResults>
-                        <!-- </div> -->
-
-                        <!-- <div class="flex flex-col gap-6">
+                        </div>
+                        <div v-else class="flex flex-col gap-6">
                             <h2 class="text-2xl font-bold text-primary-950">
                                 {{ category.name }}
                             </h2>
 
-                            <div
-                                class="flex flex-col divide-y ring-1 ring-black/5 shadow-sm px-3 pt-3 pb-1 rounded bg-white"
-                            >
-                                <h3 class="text-lg font-semibold text-gray-800 px-3 pt-1 pb-3">Articles</h3>
+                            <div>
+                                <div
+                                    class="flex flex-col divide-y ring-1 ring-black/5 shadow-sm px-3 pt-3 pb-1 rounded bg-white"
+                                >
+                                    <h3 class="text-lg font-semibold text-gray-800 px-3 pt-1 pb-3">Articles</h3>
 
-                                <div v-if="articles.length > 0">
-                                    <ul role="list" class="divide-y">
-                                        <li v-for="article in articles" :key="article.id">
-                                            <Article :article="article" />
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div v-else class="p-3 flex items-start gap-2">
-                                    <XMarkIcon class="h-5 w-5 text-gray-400" />
+                                    <div v-if="articles.length > 0">
+                                        <ul role="list" class="divide-y">
+                                            <li v-for="article in articles" :key="article.id">
+                                                <Article :article="article" />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div v-else class="p-3 flex items-start gap-2">
+                                        <XMarkIcon class="h-5 w-5 text-gray-400" />
 
-                                    <p class="text-gray-600 text-sm font-medium">No articles found in this category.</p>
+                                        <p class="text-gray-600 text-sm font-medium">
+                                            No articles found in this category.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
                     </main>
                 </div>
             </div>
