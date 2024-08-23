a9885f41f7b3e731f2d0eaea7f308d15a26c580a
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-23 20:07:07 +0000

    Ensure all module settings classes have default properties
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -7,11 +7,11 @@ arch('All Core Settings classes should have defaults for all properties')
     ->expect('App\Settings')
     ->toHaveDefaultsForAllProperties();
 
-// app(ModuleRegistry::class, [
-//     'modules_path' => 'app-modules',
-//     'cache_path' => 'cache/modules.php'
-// ])->modules()->each(function(ModuleConfig $module) {
-//     arch('app-modules-' . $module->name)
-//         ->expect($module->namespace() . 'Settings')
-//         ->toHaveAttribute('App\Settings\Contracts\HasDefaultSettings');
-// });
+app(ModuleRegistry::class, [
+    'modules_path' => 'app-modules',
+    'cache_path' => 'cache/modules.php'
+])->modules()->each(function(ModuleConfig $module) {
+    arch("All {$module->name} Settings classes should have defaults for all properties")
+        ->expect($module->namespace() . 'Settings')
+        ->toHaveAttribute('App\Settings\Contracts\HasDefaultSettings');
+});
