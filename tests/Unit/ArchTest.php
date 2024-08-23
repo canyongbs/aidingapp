99554a56600732f54ab6ed9ee565ad8c55a23159
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-08-23 20:05:58 +0000

    feat: Add arch test to ensure all Core Settings classes have defaults for all properties
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -3,7 +3,7 @@
 use InterNACHI\Modular\Support\ModuleConfig;
 use InterNACHI\Modular\Support\ModuleRegistry;
 
-arch('app')
+arch('All Core Settings classes should have defaults for all properties')
     ->expect('App\Settings')
     ->toHaveDefaultsForAllProperties();
 
