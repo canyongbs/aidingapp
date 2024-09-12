b054ea0c947ec19665fc9abfe62f6c1e37bb41f6
payal-canyon <payal.baldaniya@canyongbs.com>
2024-09-12 11:01:46 +0000

    resolved testcase issue

--- app-modules/portal/src/Settings/PortalSettings.php
+++ app-modules/portal/src/Settings/PortalSettings.php
@@ -65,7 +65,7 @@ class PortalSettings extends SettingsWithMedia
 
     public string $gdpr_banner_text = "We use cookies to personalize content, to provide social media features, and to analyze our traffic. We also share information about your use of our site with our partners who may combine it with other information that you've provided to them or that they've collected from your use of their services.";
 
-    public ?array $cookie_gdpr_banner_text;
+    public array $cookie_gdpr_banner_text = [];
 
     public GdprBannerButtonLabel $gdpr_banner_button_label = GdprBannerButtonLabel::AllowCookies;
 
