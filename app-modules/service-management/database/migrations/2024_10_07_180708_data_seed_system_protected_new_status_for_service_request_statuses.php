fe74606acc9abd13a5007a327b4bd8525d5920ce
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-10-07 18:25:44 +0000

    Fix the data migration
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/service-management/database/migrations/2024_10_07_180708_data_seed_system_protected_new_status_for_service_request_statuses.php
+++ app-modules/service-management/database/migrations/2024_10_07_180708_data_seed_system_protected_new_status_for_service_request_statuses.php
@@ -6,19 +6,32 @@ use Illuminate\Database\Migrations\Migration;
 return new class () extends Migration {
     public function up(): void
     {
-        DB::table('service_request_statuses')->upsert(
-            [
-                'name' => 'New',
-                'classification' => 'open',
-                'color' => 'info',
-                'is_system_protected' => true,
-            ],
-            [
-                'name',
-                'classification',
-            ],
-            ['is_system_protected']
-        );
+        $status = DB::table('service_request_statuses')->where([
+            'name' => 'New',
+            'classification' => 'open',
+        ])->first();
+
+        if ($status && $status->is_system_protected) {
+            return;
+        }
+
+        if ($status) {
+            DB::table('service_request_statuses')
+                ->where('name', 'New')
+                ->where('classification', 'open')
+                ->update([
+                    'is_system_protected' => true,
+                ]);
+
+            return;
+        }
+
+        DB::table('service_request_statuses')->insert([
+            'name' => 'New',
+            'classification' => 'open',
+            'color' => 'info',
+            'is_system_protected' => true,
+        ]);
     }
 
     public function down(): void
