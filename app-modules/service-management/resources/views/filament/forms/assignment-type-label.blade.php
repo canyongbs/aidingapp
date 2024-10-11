0d073a5fa9b5555db404dd5c9a870ebe81688f78
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-10-11 15:41:00 +0000

    Refactor assignment type label to use a span instead of an h2
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/service-management/resources/views/filament/forms/assignment-type-label.blade.php
+++ app-modules/service-management/resources/views/filament/forms/assignment-type-label.blade.php
@@ -31,4 +31,4 @@
 
 </COPYRIGHT>
 --}}
-<h2 class="text-xl font-semibold text-gray-950 dark:text-white sm:text-2xl">Assignment Type</h2>
+<span class="text-xl font-semibold text-gray-950 dark:text-white sm:text-2xl">Assignment Type</span>
