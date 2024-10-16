c67d6e40d235b1b3ae607354e7387157b7744f07
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-10-16 20:29:35 +0000

    Combine the column drops
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/service-management/database/migrations/2024_10_16_161945_add_notification_columns_in_service_request_types_table.php
+++ app-modules/service-management/database/migrations/2024_10_16_161945_add_notification_columns_in_service_request_types_table.php
@@ -60,19 +60,20 @@ return new class () extends Migration {
     public function down(): void
     {
         Schema::table('service_request_types', function (Blueprint $table) {
-            $table->dropColumn('is_managers_service_request_created_email_enabled');
-            $table->dropColumn('is_managers_service_request_created_notification_enabled');
-            $table->dropColumn('is_managers_service_request_assigned_email_enabled');
-            $table->dropColumn('is_managers_service_request_assigned_notification_enabled');
-            $table->dropColumn('is_managers_service_request_resolved_email_enabled');
-            $table->dropColumn('is_managers_service_request_resolved_notification_enabled');
-
-            $table->dropColumn('is_auditors_service_request_created_email_enabled');
-            $table->dropColumn('is_auditors_service_request_created_notification_enabled');
-            $table->dropColumn('is_auditors_service_request_assigned_email_enabled');
-            $table->dropColumn('is_auditors_service_request_assigned_notification_enabled');
-            $table->dropColumn('is_auditors_service_request_resolved_email_enabled');
-            $table->dropColumn('is_auditors_service_request_resolved_notification_enabled');
+            $table->dropColumn([
+                'is_managers_service_request_created_email_enabled',
+                'is_managers_service_request_created_notification_enabled',
+                'is_managers_service_request_assigned_email_enabled',
+                'is_managers_service_request_assigned_notification_enabled',
+                'is_managers_service_request_resolved_email_enabled',
+                'is_managers_service_request_resolved_notification_enabled',
+                'is_auditors_service_request_created_email_enabled',
+                'is_auditors_service_request_created_notification_enabled',
+                'is_auditors_service_request_assigned_email_enabled',
+                'is_auditors_service_request_assigned_notification_enabled',
+                'is_auditors_service_request_resolved_email_enabled',
+                'is_auditors_service_request_resolved_notification_enabled',
+            ]);
         });
     }
 };
