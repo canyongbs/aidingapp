2f2ffd16e23b20829998886797e91a478d283bd5
jayushi-canyon <jayushi.jain@canyongbs.com>
2024-10-16 20:59:09 +0530

    [AIDAPP-307]: Introduce the ability to mark an article as "Featured" for display in the client portal (#281)
    * Set Feature toggle and tag for articles
    
    * chore: fix enforcement of copyright on all files
    
    * chore: fix code style
    
    * Change column name to is_featured
    
    * chore: fix code style
    
    ---------
    
    Co-authored-by: jayushi-canyon <jayushi-canyon@users.noreply.github.com>
    Co-authored-by: joelicatajr <joelicatajr@users.noreply.github.com>

--- app-modules/knowledge-base/database/migrations/2024_10_14_102603_add_featured_column_in_knowledge_base_articles_table.php
+++ app-modules/knowledge-base/database/migrations/2024_10_14_102603_add_featured_column_in_knowledge_base_articles_table.php
@@ -1,4 +1,6 @@
-<!--
+<?php
+
+/*
 <COPYRIGHT>
 
     Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
@@ -30,26 +32,24 @@
     <https://www.canyongbs.com> or contact us via email at legal@canyongbs.com.
 
 </COPYRIGHT>
--->
-<script setup>
-    import { defineProps } from 'vue';
-    import Badge from './Badge.vue';
+*/
 
-    defineProps({
-        tags: {
-            type: Array,
-            required: true,
-        },
-        featured: {
-            type: Boolean,
-            default: false,
-        },
-    });
-</script>
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-<template>
-    <div class="flex flex-wrap gap-2">
-        <Badge v-if="featured" value="Featured" />
-        <Badge v-for="tag in tags" :key="tag.id" :value="tag.name" />
-    </div>
-</template>
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('knowledge_base_articles', function (Blueprint $table) {
+            $table->boolean('is_featured')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('knowledge_base_articles', function (Blueprint $table) {
+            $table->dropColumn('is_featured');
+        });
+    }
+};
