2382e04ff9ecf6ca2bc2b1b7c219f713c4933b58
kandarp-canyon <kandarp.pandya@canyongbs.com>
2024-10-25 00:55:58 +0530

    [AIDAPP-306]: Introduce search result pagination when using the client portal (#290)
    * [AIDAPP-306]: Introduce search result pagination when using the client portal
    
    * chore: fix enforcement of copyright on all files
    
    * chore: fix code style
    
    * Feedback changes resolved
    
    * chore: fix code style
    
    * param name changed
    
    ---------
    
    Co-authored-by: kandarp-canyon <kandarp-canyon@users.noreply.github.com>
    Co-authored-by: joelicatajr <joelicatajr@users.noreply.github.com>

--- portals/knowledge-management/src/Pages/Home.vue
+++ portals/knowledge-management/src/Pages/Home.vue
@@ -72,8 +72,15 @@
     const selectedTags = ref([]);
     const route = useRoute();
     const globalSearchInput = ref(null);
+    const currentPage = ref(1);
+    const nextPageUrl = ref(null);
+    const prevPageUrl = ref(null);
+    const lastPage = ref(null);
+    const totalArticles = ref(0);
+    const fromArticle = ref(0);
+    const toArticle = ref(0);
 
-    const debounceSearch = debounce((value) => {
+    const debounceSearch = debounce((value, page = 1) => {
         const { post } = consumer();
 
         if (!value && selectedTags.value.length < 1) {
@@ -87,15 +94,27 @@
         post(props.searchUrl, {
             search: JSON.stringify(value),
             tags: selectedTags.value.join(','),
+            page: page,
         }).then((response) => {
             searchResults.value = response.data;
             loadingResults.value = false;
+            setPagination(response.data.data.articles.meta);
         });
         globalSearchQuery.value = '';
     }, 500);
 
     const { user } = useAuthStore();
     const { hasServiceManagement } = useFeatureStore();
+
+    const setPagination = (pagination) => {
+        currentPage.value = pagination.current_page;
+        prevPageUrl.value = pagination.prev_page_url;
+        nextPageUrl.value = pagination.next_page_url;
+        lastPage.value = pagination.last_page;
+        totalArticles.value = pagination.total;
+        fromArticle.value = pagination.from;
+        toArticle.value = pagination.to;
+    };
 
     onMounted(function () {
         if (route.query.search !== undefined) {
@@ -142,6 +161,21 @@
         searchQuery.value = value;
         globalSearchInput.value.focus();
     }
+
+    const fetchNextPage = () => {
+        currentPage.value = currentPage.value !== lastPage.value ? currentPage.value + 1 : lastPage.value;
+        debounceSearch(searchQuery.value, currentPage.value);
+    };
+
+    const fetchPreviousPage = () => {
+        currentPage.value = currentPage.value !== 1 ? currentPage.value - 1 : 1;
+        debounceSearch(searchQuery.value, currentPage.value);
+    };
+
+    const fetchPage = (page) => {
+        currentPage.value = page;
+        debounceSearch(searchQuery.value, currentPage.value);
+    };
 </script>
 
 <template>
@@ -206,6 +240,14 @@
                 :searchQuery="searchQuery"
                 :searchResults="searchResults"
                 :loadingResults="loadingResults"
+                :currentPage="currentPage"
+                :lastPage="lastPage"
+                :fromArticle="fromArticle"
+                :toArticle="toArticle"
+                :totalArticles="totalArticles"
+                @fetchNextPage="fetchNextPage"
+                @fetchPreviousPage="fetchPreviousPage"
+                @fetchPage="fetchPage"
             >
             </SearchResults>
 
