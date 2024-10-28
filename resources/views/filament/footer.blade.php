ac5db105e2c0a5683dbf8e7fa03fbdd11787e52e
ketan-canyon <ketan.barevadiya@canyongbs.com>
2024-10-28 19:11:04 +0000

    Worked or PR review changes

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -48,7 +48,7 @@
 
     <div class="flex w-full justify-center pb-4 pt-2">
         <span class="w-11/12 text-center text-xs lg:w-3/4 xl:w-7/12">
-            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS&reg; and Aiding App&reg; are
+            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS&#174; and Aiding App&#174; are
             trademarks
             of Canyon GBS
             LLC. For more information or inquiries, please visit our website at <a
