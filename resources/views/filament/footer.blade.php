6ba1250421624af80981963601ad8332636a3209
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2024-10-29 19:54:24 +0530

    [AIDAPP-330]: Update the copyright text for the footer in Aiding App (#300)
    * Update the copyright text for the footer in Advising App
    
    * chore: fix code style
    
    * Worked or PR review changes
    
    ---------
    
    Co-authored-by: ketan-canyon <ketan-canyon@users.noreply.github.com>

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -48,12 +48,13 @@
 
     <div class="flex w-full justify-center pb-4 pt-2">
         <span class="w-11/12 text-center text-xs lg:w-3/4 xl:w-7/12">
-            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS™ and Aiding App™ are trademarks
+            © 2016-{{ date('Y') }} Canyon GBS LLC. All Rights Reserved. Canyon GBS&#174; and Aiding App&#174; are
+            trademarks
             of Canyon GBS
             LLC. For more information or inquiries, please visit our website at <a
                 class="text-blue-600 underline dark:text-blue-400"
                 href="https://canyongbs.com/"
-            >https://canyongbs.com/</a>.
+            >https://canyongbs.com</a>.
         </span>
     </div>
 
