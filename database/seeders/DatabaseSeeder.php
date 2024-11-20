7121637d1893b6b9d57446b5bfdb09299da647e1
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2024-11-20 23:05:36 +0000

    More deletes
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,9 +42,7 @@ use AidingApp\Task\Database\Seeders\TaskSeeder;
 use AidingApp\Team\Database\Seeders\TeamSeeder;
 use AidingApp\Alert\Database\Seeders\AlertSeeder;
 use AidingApp\Contact\Database\Seeders\ContactSeeder;
-use AidingApp\Assistant\Database\Seeders\PromptSeeder;
 use AidingApp\Division\Database\Seeders\DivisionSeeder;
-use AidingApp\Assistant\Database\Seeders\PromptTypeSeeder;
 use AidingApp\Contact\Database\Seeders\ContactSourceSeeder;
 use AidingApp\Contact\Database\Seeders\ContactStatusSeeder;
 use AidingApp\Contact\Database\Seeders\OrganizationTypeSeeder;
@@ -100,9 +98,6 @@ class DatabaseSeeder extends Seeder
             // Change Request
             ChangeRequestTypeSeeder::class,
             ChangeRequestStatusSeeder::class,
-
-            PromptTypeSeeder::class,
-            PromptSeeder::class,
 
             ContactSeeder::class,
             OrganizationTypeSeeder::class,
