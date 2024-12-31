dcfdc7b871715b62e1ccea0270196e2088beae83
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2024-12-31 16:45:19 +0000

    Worked on PR review changes

--- app-modules/service-management/database/migrations/2024_12_30_091625_add_customers_auditors_managers_fields_in_service_request_types_table.php
+++ app-modules/service-management/database/migrations/2024_12_30_091625_add_customers_auditors_managers_fields_in_service_request_types_table.php
@@ -42,14 +42,14 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('service_request_types', function (Blueprint $table) {
-            $table->boolean('is_managers_service_request_update_email_enabled')->after('is_managers_service_request_resolved_notification_enabled')->default(false);
-            $table->boolean('is_managers_service_request_update_notification_enabled')->after('is_managers_service_request_update_email_enabled')->default(false);
-            $table->boolean('is_managers_service_request_status_change_email_enabled')->after('is_managers_service_request_update_notification_enabled')->default(false);
-            $table->boolean('is_managers_service_request_status_change_notification_enabled')->after('is_managers_service_request_status_change_email_enabled')->default(false);
-            $table->boolean('is_auditors_service_request_update_email_enabled')->after('is_auditors_service_request_resolved_notification_enabled')->default(false);
-            $table->boolean('is_auditors_service_request_update_notification_enabled')->after('is_auditors_service_request_update_email_enabled')->default(false);
-            $table->boolean('is_auditors_service_request_status_change_email_enabled')->after('is_auditors_service_request_update_notification_enabled')->default(false);
-            $table->boolean('is_auditors_service_request_status_change_notification_enabled')->after('is_auditors_service_request_status_change_email_enabled')->default(false);
+            $table->boolean('is_managers_service_request_update_email_enabled');
+            $table->boolean('is_managers_service_request_update_notification_enabled')->default(false);
+            $table->boolean('is_managers_service_request_status_change_email_enabled')->default(false);
+            $table->boolean('is_managers_service_request_status_change_notification_enabled')->default(false);
+            $table->boolean('is_auditors_service_request_update_email_enabled')->default(false);
+            $table->boolean('is_auditors_service_request_update_notification_enabled')->default(false);
+            $table->boolean('is_auditors_service_request_status_change_email_enabled')->default(false);
+            $table->boolean('is_auditors_service_request_status_change_notification_enabled')->default(false);
             $table->boolean('is_customers_service_request_created_email_enabled')->default(false);
             $table->boolean('is_customers_service_request_created_notification_enabled')->default(false);
             $table->boolean('is_customers_service_request_assigned_email_enabled')->default(false);
