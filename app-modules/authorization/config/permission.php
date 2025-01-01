db875cc1ce1ea56129e43c3c54913de64e6eed91
danharrin <41773797+danharrin@users.noreply.github.com>
2025-01-01 01:46:49 +0000

    chore: update yearly copyright

--- app-modules/authorization/config/permission.php
+++ app-modules/authorization/config/permission.php
@@ -6,7 +6,7 @@ use AidingApp\Authorization\Models\Role;
 /*
 <COPYRIGHT>
 
-    Copyright © 2016-2024, Canyon GBS LLC. All rights reserved.
+    Copyright © 2016-2025, Canyon GBS LLC. All rights reserved.
 
     Aiding App™ is licensed under the Elastic License 2.0. For more details,
     see <https://github.com/canyongbs/aidingapp/blob/main/LICENSE.>
