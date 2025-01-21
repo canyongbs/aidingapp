826709b1f2bfd1231161dcd79b99aeab037511a3
Jayushi Jain <jayushi.jain@canyongbs.com>
2025-01-21 13:32:25 +0000

    Delete the old permission

---
+++ database/migrations/2025_01_21_111815_seed_permissions_delete_view_product_health_dashboard_permissions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Database\Migrations\Concerns\CanModifyPermissions;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    use CanModifyPermissions;
+
+    private array $permissions =
+        [
+            'authorization.view_product_health_dashboard ' => 'Authorization',
+        ];
+
+    private array $guards = [
+        'web',
+        'api',
+    ];
+
+    public function up(): void
+    {
+        collect($this->guards)
+            ->each(fn (string $guard) => $this->deletePermissions(array_keys($this->permissions), $guard));
+    }
+
+    public function down(): void
+    {
+        collect($this->guards)
+            ->each(function (string $guard) {
+                $this->createPermissions($this->permissions, $guard);
+            });
+    }
+};
