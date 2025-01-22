cd96a6ed4dd46d985bd11c28852127b70d141b13
Payal Baldaniya <payal.baldaniya@canyongbs.com>
2025-01-22 19:10:25 +0530

    [AIDAPP-420]: Remove the Full-Width display option for the client portal (#391)
    * [AIDAPP-420]: Remove the Full-Width display option for the client portal
    
    * chore: fix enforcement of copyright on all files
    
    ---------
    
    Co-authored-by: payal-canyon <payal-canyon@users.noreply.github.com>

--- app-modules/portal/database/migrations/2025_01_20_091920_delete_knowledge_management_portal_layout_portal_setting.php
+++ app-modules/portal/database/migrations/2025_01_20_091920_delete_knowledge_management_portal_layout_portal_setting.php
@@ -34,21 +34,16 @@
 </COPYRIGHT>
 */
 
-namespace AidingApp\Portal\Enums;
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
 
-use Filament\Support\Contracts\HasLabel;
-
-enum PortalLayout: string implements HasLabel
-{
-    case Full = 'full';
-
-    case Fixed = 'fixed';
-
-    public function getLabel(): ?string
+return new class () extends SettingsMigration {
+    public function up(): void
     {
-        return match ($this) {
-            PortalLayout::Full => 'Full-Width',
-            PortalLayout::Fixed => 'Fixed-Width (Boxed Layout)',
-        };
+        $this->migrator->deleteIfExists('portal.knowledge_management_portal_layout');
     }
-}
+
+    public function down(): void
+    {
+        $this->migrator->add('portal.knowledge_management_portal_layout', null);
+    }
+};
