71e5edb05fa9d7991d7a3bbca556247491b199fd
Orrison <Orrison@users.noreply.github.com>
2025-02-26 18:41:25 +0000

    chore: fix code style

--- app-modules/service-management/database/migrations/2025_02_26_180821_data_activate_incident_update_feature.php
+++ app-modules/service-management/database/migrations/2025_02_26_180821_data_activate_incident_update_feature.php
@@ -37,8 +37,7 @@
 use App\Features\IncidentUpdateFeature;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         IncidentUpdateFeature::activate();
