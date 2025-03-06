08f16d92db2aac2aa071615d49237a2d4f6578e0
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-03-06 00:53:21 +0000

    Add the mail config value
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- config/mail.php
+++ config/mail.php
@@ -133,6 +133,7 @@ return [
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'no-reply@aiding.app'),
         'name' => env('MAIL_FROM_NAME', 'Aiding App™'),
+        'root_domain' => env('MAIL_ROOT_DOMAIN', 'mail.aiding.app'),
     ],
 
     /*
