e354706c98b6ce51e8764af5a15b39da09e794b1
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-03-11 03:35:41 +0000

    Remove engagement deliverable permissions and update message models activation process
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/notification/database/migrations/2025_03_06_135637_tmp_data_activate_new_message_models_feature_flag.php
+++ app-modules/notification/database/migrations/2025_03_06_135637_tmp_data_activate_new_message_models_feature_flag.php
@@ -2,11 +2,16 @@
 
 use App\Features\NewMessageModels;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
 
 return new class () extends Migration {
     public function up(): void
     {
         NewMessageModels::activate();
+
+        Artisan::call('queue:restart');
+        Artisan::call('schedule:interrupt');
+        Artisan::call('schedule:clear-cache');
     }
 
     public function down(): void
