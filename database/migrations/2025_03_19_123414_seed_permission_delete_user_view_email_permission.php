06fb85c35d8769705e5ab81c72dfb0e2037df85e
Kandarp Pandya <kandarp.pandya@canyongbs.com>
2025-03-19 12:53:14 +0000

    [AIDAPP-478]: Control API access to user email addresses

---
+++ database/migrations/2025_03_19_123414_seed_permission_delete_user_view_email_permission.php
@@ -0,0 +1,30 @@
+<?php
+
+use Database\Migrations\Concerns\CanModifyPermissions;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    use CanModifyPermissions;
+
+    private array $permissions = [
+        'user.view-email' => 'User',
+    ];
+
+    private array $guards = [
+        'api',
+    ];
+
+    public function up(): void
+    {
+        collect($this->guards)
+            ->each(fn (string $guard) => $this->deletePermissions(array_keys($this->permissions), $guard));
+    }
+
+    public function down(): void
+    {
+        collect($this->guards)
+            ->each(function (string $guard) {
+                $this->createPermissions($this->permissions, $guard);
+            });
+    }
+};
