1768df91a2da1b9250d550eaecd956e0c348aab6
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2025-03-28 14:14:28 +0000

    refactor: rename 'Replicate' to 'Duplicate' in actions and notifications for clarity

---
+++ resources/lang/vendor/filament-actions/en/replicate.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'single' => [
+        'label' => 'Duplicate',
+
+        'modal' => [
+            'heading' => 'Duplicate :label',
+
+            'actions' => [
+                'replicate' => [
+                    'label' => 'Duplicate',
+                ],
+            ],
+        ],
+
+        'notifications' => [
+            'Duplicated' => [
+                'title' => 'Duplicated',
+            ],
+        ],
+    ],
+];
