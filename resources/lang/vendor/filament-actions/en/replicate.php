f03cdabdf1b07c75e02a9ad10e9bd91a09e5b50d
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2025-03-28 14:15:43 +0000

    refactor: update notification key from 'Duplicated' to 'replicate' for consistency

--- resources/lang/vendor/filament-actions/en/replicate.php
+++ resources/lang/vendor/filament-actions/en/replicate.php
@@ -15,7 +15,7 @@ return [
         ],
 
         'notifications' => [
-            'Duplicated' => [
+            'replicate' => [
                 'title' => 'Duplicated',
             ],
         ],
