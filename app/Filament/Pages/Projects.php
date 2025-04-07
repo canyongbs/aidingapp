889d45cca4427811ef550590f17961f7c4584e8d
Kandarp Pandya <kandarp.pandya@canyongbs.com>
2025-04-07 13:38:13 +0000

    Navigation added for Projects

---
+++ app/Filament/Pages/Projects.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Projects extends Page
+{
+    protected static string $view = 'filament.pages.coming-soon';
+
+    protected static ?string $navigationLabel = 'Projects';
+
+    protected static ?string $navigationGroup = 'Project Management';
+}
