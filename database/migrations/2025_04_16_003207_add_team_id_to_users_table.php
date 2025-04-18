af07b79c4db3a8f6bf9f93e4af08527c0cd9311d
Orrison <Orrison@users.noreply.github.com>
2025-04-18 15:18:04 +0000

    chore: fix code style

--- database/migrations/2025_04_16_003207_add_team_id_to_users_table.php
+++ database/migrations/2025_04_16_003207_add_team_id_to_users_table.php
@@ -34,13 +34,11 @@
 </COPYRIGHT>
 */
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
--- database/migrations/2025_04_16_195003_migrate_team_user_id_to_users_table.php
+++ database/migrations/2025_04_16_195003_migrate_team_user_id_to_users_table.php
@@ -34,11 +34,10 @@
 </COPYRIGHT>
 */
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         DB::table('team_user')->get()->each(function ($teamUser) {
