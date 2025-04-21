694362f1484990db84c6ad1f35a65f86d82eadd8
Ketan Barevadiya <ketan.barevadiya@canyongbs.com>
2025-04-21 11:01:59 +0000

    Remove 'Knowledge Base' route and related components from the portal

--- portals/knowledge-management/src/Components/Header.vue
+++ portals/knowledge-management/src/Components/Header.vue
@@ -96,11 +96,6 @@
             command: () => router.push({ name: 'incidents' }),
         },
         {
-            label: 'Knowledge Base',
-            routeName: 'knowledge-base',
-            command: () => router.push({ name: 'knowledge-base' }),
-        },
-        {
             label: 'Assets',
             routeName: 'assets',
             visible: hasAssets,
