cadd111cfafbbf0e0cb47ee3229636dff2d56e67
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-05-06 18:13:12 +0000

    Create permission enum

---
+++ app/Enums/PermissionDescription.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Enums;
+
+enum PermissionDescription: string
+{
+    case Alert = 'This permission group enables the display and management of the Alert feature in the tertiary navigation group View Contact. Permission options include create, view, update, and delete alerts.';
+    case Asset = 'This permission group enables the display and management of the Asset feature in the primary navigation group Service Management. Permission options include create, view, update, and delete assets.';
+    case AssetCheckIn = 'This permission group enables the display and management of the Asset Check in feature, a primary navigation group. Permission options include view asset check ins. Note: Users cannot access this feature.';
+    case AssetCheckOut = 'This permission group enables the display and management of the Asset Check out feature, a primary navigation group. Permission options include view asset check outs. Note: Users cannot access this feature.';
+    case AssetLocation = 'This permission group enables the display and management of the Asset Location feature in the secondary navigation group Asset Management. Permission options include create, view, update, and delete asset locations.';
+    case AssetStatus = 'This permission group enables the display and management of the Asset Status feature in the secondary navigation group Asset Management. Permission options include create, view, update, and delete asset statuses.';
+    case AssetType = 'This permission group enables the display and management of the Asset Type feature in the secondary navigation group Asset Management. Permission options include create, view, update, and delete asset types.';
+    case Audit = 'This permission group enables the display and management of the Audit feature in the secondary navigation group Usage Auditing. Permission options include  view audits.';
+    case ChangeRequest = 'This permission group enables the display and management of the Change Request feature in the primary navigation group Service Management. Permission options include create, view, update, and delete change requests.';
+    case ChangeRequestStatus = 'This permission group enables the display and management of the Change Request Status feature in the secondary navigation group Service Management. Permission options include create, view, update, and delete change request statuses.';
+    case ChangeRequestType = 'This permission group enables the display and management of the Change Request Type feature in the secondary navigation group Service Management. Permission options include create, view, update, and delete change request types.';
+    case Contact = 'This permission group enables the display and management of the Contact feature in the primary navigation group Clients. Permission options include create, view, update, and delete contacts.';
+    case Contract = 'This permission group enables the display and management of the Contract feature in the primary navigation group Purchasing. Permission options include create, view, update, and delete contracts.';
+    case Division = 'This permission group enables the display and management of the Division feature in the primary navigation group User Management. Permission options include create, view, update, and delete divisions.';
+    case Engagement = 'This permission group enables the display of the Timeline feature in various navigation groups. Permission options include view timelines.';
+    case EngagementFile = 'This permission group enables the display and management of the Engagement File feature in the primary navigation group Engagement Features. Permission options include create, view, update, and delete engagement files. Note: Users cannot access this feature.';
+    case EngagementResponse = 'This permission group enables the display of the Engagement Response feature. Permission options include view engagement responses. Note: Users cannot access this feature.';
+    case Incident = 'This permission group enables the display and management of the Incident feature in the primary navigtion group Service Management. Permission options include create, view, update, and delete incidents.';
+    case IncidentUpdate = 'This permission group enables the display and management of the Incident Update feature in the tertiary navigation group View Incident. Permission options include create, view, update, and delete incident updates.';
+    case KnowledgeBaseItem = 'This permission group enables the display and management of the Knowledge Base Item feature in the primary navigation group Service Management. Permission options include create, view, update, and delete knowledge base items.';
+    case License = 'This permission group enables the management of the License feature. Permission options include bulk assigning licesnes.';
+    case MaintenanceActivity = 'This permission group enables the display and management of the Maintenance Activity feature in the tertiary navigation group View Asset. Permission options include create, view, and update maintenance activity.';
+    case Organization = 'This permission group enables the display and management of the Organization feature in the primary navigation group Clients. Permission options include create, view, update, and delete organizations.';
+    case Permission = 'This permission group enables the display and management of the Permission feature in the primary navigation group User Management. Permission options include view permissions.';
+    case Product = 'This permission group enables the display and management of the License Management feature in the primary navigation group Purchasing. Permission options include create, view, update, and delete products.';
+    case ProductAdmin = 'This permission group enables access of the primary navigation group Product Administration.';
+    case ProductLicense = 'This permission group enables the display and management of the Product License feature in the tertiary navigation group View License. Permission options include create, view, update, and delete product licenses.';
+    case Project = 'This permission group enables the display of the Project feature in the primary navigation group Projects. It is coming soon. Permission options will include create, view, update, and delete';
+    case RealtimeChat = 'This permission group enables the display of the Realtime Chat feature in the primary navigation group Staff Engagement. Permission options include view realtime chat.';
+    case ReportLibrary = 'This permission group enables the display of the Report Library feature in the primary navigation group Report Center. Permission options include view report libraries.';
+    case Role = 'This permission group enables the display and management of the Role feature in the primary navigation group User Management. Permission options include create, view, update, and delete roles.';
+    case ServiceMonitoring = 'This permission group enables the display and management of the Service Monitoring feature in the primary navigation group Service Management. Permission options include create, view, update, and delete service monitorings.';
+    case ServiceRequest = 'This permission group enables the display and management of the Service Request feature in the primary navigation group Service Management. Permission options include create, view, update, and delete service requests.';
+    case ServiceRequestAssignment = 'This permission group enables the display of the Service Request Assignment feature in the tertiary navigation group View Service Request. Permission options include create, view, update, and delete service request assignments.';
+    case ServiceRequestHistory = 'This permission group enables the display of the Service Request History feature in the tertiary navigation group View Service Request. Permission options include view service history.';
+    case ServiceRequestPriority = 'This permission group enables the display and management of the Service Request Priority feature in the tertiary navigation group View Service Request. Permission options include create, view, update, and delete service request priorities.';
+    case ServiceRequestUpdate = 'This permission group enables the display of the Service Request Update feature in the tertiary navigation group View Service Request. Permission options include create, view, update, and delete service request updates.';
+    case Subscription = 'This permission group enables the display of the Subscription feature in the tertiary navigation group View Contact. Permission options include create and delete subscriptions. Note: This feature is currently gated by User permissions.';
+    case SystemUser = 'This permission group enables the display and management of the Programmatic Users feature in the primary navigation group User Management. Permission options include create, view, update, and delete system users.';
+    case Task = 'This permission group enables the display and management of the Task feature in the primary navigation group Project Management. Permission options include create, view, update, and delete tasks.';
+    case Team = 'This permission group enables the display and management of the Team feature in the primary navigation group User Management. Permission options include create, view, update, and delete teams.';
+    case User = 'This permission group enables the display and management of the User feature in the primary navigation group User Management. Permission options include create, view, update, and delete users.';
+
+    public function getLabel(): string
+    {
+        return $this->name;
+    }
+
+}
