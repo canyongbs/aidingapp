28e94f3b2589b05e93320c64ea7304b9e8063adb
Orrison <Orrison@users.noreply.github.com>
2025-05-14 21:52:56 +0000

    chore: fix code style

--- app-modules/service-management/src/Models/ServiceRequest.php
+++ app-modules/service-management/src/Models/ServiceRequest.php
@@ -60,7 +60,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Support\Facades\DB;
