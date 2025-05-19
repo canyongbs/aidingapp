f39f0ec8367ed2a175f608c481dde60ecfa755db
Orrison <Orrison@users.noreply.github.com>
2025-05-19 17:00:45 +0000

    chore: fix code style

--- app-modules/alert/database/migrations/2025_05_19_164014_alter_table_alerts_change_conern_id.php
+++ app-modules/alert/database/migrations/2025_05_19_164014_alter_table_alerts_change_conern_id.php
@@ -36,11 +36,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
-use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         DB::beginTransaction();
