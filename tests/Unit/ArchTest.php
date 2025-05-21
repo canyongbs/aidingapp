5eb61d2d03fbb9abf563bb4828775a6a2fe3ce6d
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-05-21 23:25:32 +0000

    Rearrange arch tests
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -37,7 +37,6 @@
 use App\Concerns\EditPageRedirection;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 use InterNACHI\Modular\Support\ModuleConfig;
 use InterNACHI\Modular\Support\ModuleRegistry;
 use PHPUnit\Framework\Assert;
@@ -46,6 +45,11 @@ arch('All Core Settings classes should have defaults for all properties')
     ->expect('App\Settings')
     ->toHaveDefaultsForAllProperties();
 
+arch('All Core Models should not use HasUuids trait')
+    ->expect('App\Models')
+    ->extending(Model::class)
+    ->not->toUseTrait('Illuminate\Database\Eloquent\Concerns\HasUuids');
+
 app(ModuleRegistry::class, [
     'modules_path' => 'app-modules',
     'cache_path' => 'cache/modules.php',
@@ -53,6 +57,11 @@ app(ModuleRegistry::class, [
     arch("All {$module->name} Settings classes should have defaults for all properties")
         ->expect($module->namespace() . 'Settings')
         ->toHaveDefaultsForAllProperties();
+
+    arch("All {$module->name} Models should not use HasUuids trait")
+        ->expect($module->namespace() . 'Models')
+        ->extending(Model::class)
+        ->not->toUseTrait('Illuminate\Database\Eloquent\Concerns\HasUuids');
 });
 
 test('pages extending EditRecord have the EditPageRedirection test', function () {
@@ -74,22 +83,4 @@ test('pages extending EditRecord have the EditPageRedirection test', function ()
             "Class [{$class}] does not use the EditPageRedirection trait.",
         );
     }
-});
-
-arch('All Core Models should not use HasUuids trait')
-    ->expect('App\Models')
-    ->extending(Model::class)
-    ->not->toUseTrait('Illuminate\Database\Eloquent\Concerns\HasUuids');
-
-/** @var Collection<int, ModuleConfig> $modules */
-$modules = app(ModuleRegistry::class, [
-    'modules_path' => 'app-modules',
-    'cache_path' => 'cache/modules.php',
-])->modules();
-
-$modules->each(function (ModuleConfig $module) {
-    arch("All {$module->name} Models should not use HasUuids trait")
-        ->expect($module->namespace() . 'Models')
-        ->extending(Model::class)
-        ->not->toUseTrait('Illuminate\Database\Eloquent\Concerns\HasUuids');
 });
