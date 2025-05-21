68f4c2708a53cbbd0ab44c9a87373903c52bacac
rebecca-canyon <rebecca-canyon@users.noreply.github.com>
2025-05-21 16:23:46 +0000

    chore: fix code style

--- app-modules/contact/src/Models/Contact.php
+++ app-modules/contact/src/Models/Contact.php
@@ -66,7 +66,6 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
