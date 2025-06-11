d90f002656d4aecc250249c8a865970c3f4dd985
jayushi-canyon <170343024+jayushi-canyon@users.noreply.github.com>
2025-06-11 17:25:35 +0000

    chore: fix code style

--- app-modules/portal/routes/api.php
+++ app-modules/portal/routes/api.php
@@ -70,7 +70,7 @@ Route::prefix('api')
     ])
     ->group(function () {
         Route::middleware(['auth:sanctum'])->group(function () {
-            Route::get('/user', fn() => auth('contact')->user() ?? response()->json(status: Response::HTTP_UNAUTHORIZED))
+            Route::get('/user', fn () => auth('contact')->user() ?? response()->json(status: Response::HTTP_UNAUTHORIZED))
                 ->name('user.auth-check');
         });
 
