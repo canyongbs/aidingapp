f3e6f14c78ed794b11cf8cbb012eb1f8b1fe0dba
Orrison <6799341+Orrison@users.noreply.github.com>
2025-06-18 20:20:11 +0000

    chore: fix code style

--- app-modules/task/resources/views/filament/pages/list-tasks.blade.php
+++ app-modules/task/resources/views/filament/pages/list-tasks.blade.php
@@ -39,24 +39,41 @@
     'fi-resource-' . str_replace('/', '-', $this->getResource()::getSlug()),
 ])>
     <div class="flex w-full justify-start">
-        <div class="grid max-w-xs grid-cols-2 gap-1 rounded-lg bg-gray-100 p-1 dark:bg-gray-800" role="group">
-            <button type="button" @class([
-                'px-5 py-1.5 text-xs font-medium rounded-lg',
-                'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900' =>
-                    $viewType === 'table',
-                'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' =>
-                    $viewType !== 'table',
-            ]) wire:click="setViewType('table')">
-                <x-filament::icon class="h-6 w-6" icon="heroicon-m-table-cells" />
+        <div
+            class="grid max-w-xs grid-cols-2 gap-1 rounded-lg bg-gray-100 p-1 dark:bg-gray-800"
+            role="group"
+        >
+            <button
+                type="button"
+                @class([
+                    'px-5 py-1.5 text-xs font-medium rounded-lg',
+                    'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900' =>
+                        $viewType === 'table',
+                    'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' =>
+                        $viewType !== 'table',
+                ])
+                wire:click="setViewType('table')"
+            >
+                <x-filament::icon
+                    class="h-6 w-6"
+                    icon="heroicon-m-table-cells"
+                />
             </button>
-            <button type="button" @class([
-                'px-5 py-1.5 text-xs font-medium rounded-lg',
-                'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900' =>
-                    $viewType === 'kanban',
-                'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' =>
-                    $viewType !== 'kanban',
-            ]) wire:click="setViewType('kanban')">
-                <x-filament::icon class="h-6 w-6" icon="heroicon-m-view-columns" />
+            <button
+                type="button"
+                @class([
+                    'px-5 py-1.5 text-xs font-medium rounded-lg',
+                    'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900' =>
+                        $viewType === 'kanban',
+                    'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' =>
+                        $viewType !== 'kanban',
+                ])
+                wire:click="setViewType('kanban')"
+            >
+                <x-filament::icon
+                    class="h-6 w-6"
+                    icon="heroicon-m-view-columns"
+                />
             </button>
         </div>
     </div>
@@ -72,9 +89,13 @@
                             $tabKey = strval($tabKey);
                         @endphp
 
-                        <x-filament::tabs.item :active="$activeTab === $tabKey" :badge="$tab->getBadge()" :icon="$tab->getIcon()"
+                        <x-filament::tabs.item
+                            :active="$activeTab === $tabKey"
+                            :badge="$tab->getBadge()"
+                            :icon="$tab->getIcon()"
                             :icon-position="$tab->getIconPosition()"
-                            :wire:click="'$set(\'activeTab\', ' . (filled($tabKey) ? ('\'' . $tabKey . '\'') : 'null') . ')'">
+                            :wire:click="'$set(\'activeTab\', ' . (filled($tabKey) ? ('\'' . $tabKey . '\'') : 'null') . ')'"
+                        >
                             {{ $tab->getLabel() ?? $this->generateTabLabel($tabKey) }}
                         </x-filament::tabs.item>
                     @endforeach
