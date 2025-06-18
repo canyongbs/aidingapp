215af55d2910316ae95db35f38be037934013996
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-06-18 20:18:46 +0000

    Fix issues
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/authorization/src/Http/Controllers/SocialiteController.php
+++ app-modules/authorization/src/Http/Controllers/SocialiteController.php
@@ -43,6 +43,8 @@ use App\Models\User;
 use Filament\Facades\Filament;
 use Filament\Notifications\Notification;
 use Illuminate\Database\Query\Expression;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -95,9 +97,10 @@ class SocialiteController extends Controller
         if ($provider === SocialiteProvider::Azure) {
             try {
                 $request = Http::withToken($socialiteUser->token)
+                    ->dontTruncateExceptions()
                     ->retry(3, 500)
                     ->get('https://graph.microsoft.com/v1.0/me/photo/$value')
-                    ->throw();
+                    ->throwIf(fn (Response $response) => $response->failed() && $response->status() !== 404);
 
                 $mimeType = $request->header('Content-Type');
 
@@ -111,6 +114,10 @@ class SocialiteController extends Controller
                     }
                 } else {
                     throw new InvalidUserAvatarMimeType($mimeType, $user);
+                }
+            } catch (RequestException $e) {
+                if (! str_contains($e->getMessage(), 'Microsoft.Fast.Profile.Core.Exception.ImageNotFoundException')) {
+                    report($e);
                 }
             } catch (Throwable $e) {
                 report($e);
