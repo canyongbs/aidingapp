34a1eb1009a46617b3e37c36bd754a1c07b99dcf
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-06-23 19:08:26 +0000

    Remove API docs
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/profiles/{user:public_profile_slug}', ViewPublicUserProfileController::class)
     ->name('users.profile.view.public');
-
-Route::middleware(['auth', 'can:product_admin.view-any', 'can:product_admin.*.view'])
-    ->get('/api-docs', fn () => File::get(storage_path('app/api-docs/index.html')))
-    ->name('api-docs');
