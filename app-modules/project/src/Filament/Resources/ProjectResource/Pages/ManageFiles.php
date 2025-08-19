88b97bcc221119b565bb488aad6550727b5f3353
Karl Murray <karl.murray@canyongbs.com>
2025-08-19 15:25:03 +0000

    [AIDAPP-676]: Add the Retention Date to the Project Files table.

--- app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageFiles.php
+++ app-modules/project/src/Filament/Resources/ProjectResource/Pages/ManageFiles.php
@@ -161,6 +161,11 @@ class ManageFiles extends ManageRelatedRecords
                         'video/mp4' => 'heroicon-o-video-camera',
                         'application/zip' => 'heroicon-o-archive-box'
                     }),
+                TextColumn::make('retention_date')
+                    ->label('Retention Date')
+                    ->placeholder('N/A')
+                    ->dateTime()
+                    ->sortable(query: fn ($query, $direction) => $query->orderBy('project_files.retention_date', $direction)),
                 TextColumn::make('created_at')
                     ->label('Date Created')
                     ->dateTime()
