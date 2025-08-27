67c88ba0fa585c2dac2d9d077fa6a49e2231c089
krlmrr <34105282+krlmrr@users.noreply.github.com>
2025-08-27 18:27:17 +0000

    Update code formatting and copyright headers

--- app-modules/contact/src/Filament/Resources/ContactResource/Pages/ManageContactTasks.php
+++ app-modules/contact/src/Filament/Resources/ContactResource/Pages/ManageContactTasks.php
@@ -36,26 +36,26 @@
 
 namespace AidingApp\Contact\Filament\Resources\ContactResource\Pages;
 
-use App\Models\User;
-use Filament\Tables\Table;
-use AidingApp\Task\Models\Task;
-use App\Models\Scopes\HasLicense;
-use Filament\Tables\Filters\Filter;
-use AidingApp\Task\Enums\TaskStatus;
-use AidingApp\Contact\Models\Contact;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Columns\TextColumn;
-use Illuminate\Database\Eloquent\Model;
-use App\Filament\Resources\UserResource;
-use App\Features\ConfidentialTaskFeature;
-use App\Filament\Tables\Columns\IdColumn;
-use Filament\Tables\Actions\CreateAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Filters\SelectFilter;
-use Illuminate\Contracts\Database\Query\Builder;
 use AidingApp\Contact\Filament\Resources\ContactResource;
+use AidingApp\Contact\Models\Contact;
+use AidingApp\Task\Enums\TaskStatus;
 use AidingApp\Task\Filament\RelationManagers\BaseTaskRelationManager;
 use AidingApp\Task\Filament\Resources\TaskResource\Components\TaskViewAction;
+use AidingApp\Task\Models\Task;
+use App\Features\ConfidentialTaskFeature;
+use App\Filament\Resources\UserResource;
+use App\Filament\Tables\Columns\IdColumn;
+use App\Models\Scopes\HasLicense;
+use App\Models\User;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 class ManageContactTasks extends BaseTaskRelationManager
 {
