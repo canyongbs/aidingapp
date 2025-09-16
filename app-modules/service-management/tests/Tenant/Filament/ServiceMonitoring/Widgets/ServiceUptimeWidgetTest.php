64063e06a8e3f4f8e19adf37b95b1ecd9b9d959a
Rebecca Sheridan <rebecca.sheridan@canyongbs.com>
2025-09-16 04:51:32 +0000

    Scaffold tests

---
+++ app-modules/service-management/tests/Tenant/Filament/ServiceMonitoring/Widgets/ServiceUptimeWidgetTest.php
@@ -0,0 +1,14 @@
+<?php
+
+it('displays the correct uptime percentage', function () {
+    // make factories
+
+    // $stats = (new Widget())->getStats();
+
+    // expect($stats[0]->getValue())->toBe(3)
+    //     ->and($stats[1]->getValue())->toBe(2);
+})->todo();
+
+it('displays N/A if there is not data for that period', function () {
+
+})->todo();
