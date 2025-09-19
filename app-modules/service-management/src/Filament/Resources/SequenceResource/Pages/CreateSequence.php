8dab7847ae129b8180f6a9b6abb5025b5129d349
Karl Murray <karl.murray@canyongbs.com>
2025-09-19 20:19:21 +0000

    [AIDAPP-700]: Fix broken use statement

--- app-modules/service-management/src/Filament/Resources/SequenceResource/Pages/CreateSequence.php
+++ app-modules/service-management/src/Filament/Resources/SequenceResource/Pages/CreateSequence.php
@@ -36,6 +36,7 @@
 
 namespace AidingApp\ServiceManagement\Filament\Resources\SequenceResource\Pages;
 
+use AidingApp\ServiceManagement\Filament\Resources\SequenceResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSequence extends CreateRecord
