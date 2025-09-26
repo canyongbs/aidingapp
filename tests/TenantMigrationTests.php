0f053dd13b97b618cdbd2f92cd286ea46ace31b8
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-09-26 20:23:47 +0000

    Move to a describe block
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- tests/TenantMigrationTests.php
+++ tests/TenantMigrationTests.php
@@ -11,50 +11,52 @@ use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-test('2025_09_26_183417_tmp_data_backfill_service_request_update_created_by', function () {
-    isolatedMigration(
-        '2025_09_26_183417_tmp_data_backfill_service_request_update_created_by',
-        function () {
-            $user = User::factory()->create();
+describe('2025_09_26_183417_tmp_data_backfill_service_request_update_created_by', function () {
+    it('can set createdBy for a outbound direction when there is an assigned User', function () {
+        isolatedMigration(
+            '2025_09_26_183417_tmp_data_backfill_service_request_update_created_by',
+            function () {
+                $user = User::factory()->create();
 
-            $team = Team::factory()->create();
+                $team = Team::factory()->create();
 
-            $user->team()->associate($team);
-            $user->save();
+                $user->team()->associate($team);
+                $user->save();
 
-            $serviceRequest = ServiceRequest::factory()
-                ->for(
-                    ServiceRequestPriority::factory()
-                        ->for(
-                            ServiceRequestType::factory()
-                                ->hasAttached(
-                                    factory: $team,
-                                    relationship: 'managers'
-                                ),
-                            'type',
-                        ),
-                    'priority'
-                )
-                ->create();
+                $serviceRequest = ServiceRequest::factory()
+                    ->for(
+                        ServiceRequestPriority::factory()
+                            ->for(
+                                ServiceRequestType::factory()
+                                    ->hasAttached(
+                                        factory: $team,
+                                        relationship: 'managers'
+                                    ),
+                                'type',
+                            ),
+                        'priority'
+                    )
+                    ->create();
 
-            $serviceRequest->assignments()->create([
-                'user_id' => $user->id,
-                'assigned_by_id' => null,
-                'assigned_at' => now(),
-                'status' => ServiceRequestAssignmentStatus::Active,
-            ]);
+                $serviceRequest->assignments()->create([
+                    'user_id' => $user->id,
+                    'assigned_by_id' => null,
+                    'assigned_at' => now(),
+                    'status' => ServiceRequestAssignmentStatus::Active,
+                ]);
 
-            $serviceRequestUpdate = ServiceRequestUpdate::factory()
-                ->for($serviceRequest, 'serviceRequest')
-                ->create(['direction' => ServiceRequestUpdateDirection::Outbound]);
+                $serviceRequestUpdate = ServiceRequestUpdate::factory()
+                    ->for($serviceRequest, 'serviceRequest')
+                    ->create(['direction' => ServiceRequestUpdateDirection::Outbound]);
 
-            expect($serviceRequestUpdate->createdBy)->toBeNull();
+                expect($serviceRequestUpdate->createdBy)->toBeNull();
 
-            $migrate = Artisan::call('migrate', ['--path' => 'app-modules/service-management/database/migrations/2025_09_26_183417_tmp_data_backfill_service_request_update_created_by.php']);
+                $migrate = Artisan::call('migrate', ['--path' => 'app-modules/service-management/database/migrations/2025_09_26_183417_tmp_data_backfill_service_request_update_created_by.php']);
 
-            expect($migrate)->toBe(Command::SUCCESS);
+                expect($migrate)->toBe(Command::SUCCESS);
 
-            expect($serviceRequestUpdate->refresh()->createdBy->is($user))->toBeTrue();
-        }
-    );
+                expect($serviceRequestUpdate->refresh()->createdBy->is($user))->toBeTrue();
+            }
+        );
+    });
 });
