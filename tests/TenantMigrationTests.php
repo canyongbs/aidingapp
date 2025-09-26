17d8a0934eaaccb6d796727b3733d6df542b0ce0
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2025-09-26 20:21:22 +0000

    Start tests for and fix data migration
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- tests/TenantMigrationTests.php
+++ tests/TenantMigrationTests.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Enums\Locality;
-use App\Models\Client;
+use AidingApp\ServiceManagement\Enums\ServiceRequestAssignmentStatus;
+use AidingApp\ServiceManagement\Enums\ServiceRequestUpdateDirection;
+use AidingApp\ServiceManagement\Models\ServiceRequest;
+use AidingApp\ServiceManagement\Models\ServiceRequestPriority;
+use AidingApp\ServiceManagement\Models\ServiceRequestType;
+use AidingApp\ServiceManagement\Models\ServiceRequestUpdate;
+use AidingApp\Team\Models\Team;
+use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
@@ -9,17 +15,46 @@ test('2025_09_26_183417_tmp_data_backfill_service_request_update_created_by', fu
     isolatedMigration(
         '2025_09_26_183417_tmp_data_backfill_service_request_update_created_by',
         function () {
-            // $locality = fake()->randomElement(Locality::cases());
+            $user = User::factory()->create();
 
-            // assert($locality instanceof Locality);
+            $team = Team::factory()->create();
 
-            // $client = Client::factory()->createQuietly(['state' => $locality->getLabel()]);
+            $user->team()->associate($team);
+            $user->save();
+
+            $serviceRequest = ServiceRequest::factory()
+                ->for(
+                    ServiceRequestPriority::factory()
+                        ->for(
+                            ServiceRequestType::factory()
+                                ->hasAttached(
+                                    factory: $team,
+                                    relationship: 'managers'
+                                ),
+                            'type',
+                        ),
+                    'priority'
+                )
+                ->create();
+
+            $serviceRequest->assignments()->create([
+                'user_id' => $user->id,
+                'assigned_by_id' => null,
+                'assigned_at' => now(),
+                'status' => ServiceRequestAssignmentStatus::Active,
+            ]);
+
+            $serviceRequestUpdate = ServiceRequestUpdate::factory()
+                ->for($serviceRequest, 'serviceRequest')
+                ->create(['direction' => ServiceRequestUpdateDirection::Outbound]);
+
+            expect($serviceRequestUpdate->createdBy)->toBeNull();
 
             $migrate = Artisan::call('migrate', ['--path' => 'app-modules/service-management/database/migrations/2025_09_26_183417_tmp_data_backfill_service_request_update_created_by.php']);
 
             expect($migrate)->toBe(Command::SUCCESS);
 
-            // expect($client->refresh()->state)->toBe($locality->value);
+            expect($serviceRequestUpdate->refresh()->createdBy->is($user))->toBeTrue();
         }
     );
 });
