4c112dd0f0edd99691e71697cf2b410f8da6178f
Kevin Ullyott <Orrison@users.noreply.github.com>
2025-10-23 19:22:47 -0400

    [AIDAPP-731]: Deprecate legacy settings in the configuration panel (#701)
    * Remove no longer needed email settings
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>
    
    * Hide s3 setting when not local
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>
    
    * Remove old tests
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>
    
    ---------
    
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app-modules/integration-aws-ses-event-handling/tests/Tenant/Feature/Filament/ManageAmazonSesSettingsTest.php
+++ app-modules/integration-aws-ses-event-handling/tests/Tenant/Feature/Filament/ManageAmazonSesSettingsTest.php
@@ -35,12 +35,10 @@
 */
 
 use AidingApp\IntegrationAwsSesEventHandling\Filament\Pages\ManageAmazonSesSettings;
-use App\Models\Tenant;
 use App\Models\User;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
-use function Pest\Livewire\livewire;
 use function Tests\asSuperAdmin;
 
 it('prevents access to the Amazon SES Settings when you do not have the necessary permissions', function () {
@@ -59,64 +57,4 @@ it('allows access to the Amazon SES Settings when you do have the necessary perm
 
     get(ManageAmazonSesSettings::getUrl())
         ->assertOk();
-});
-
-it('renders the correct Amazon SES settings for the Tenant', function () {
-    $user = User::factory()->create();
-
-    asSuperAdmin($user);
-
-    /** @var Tenant $tenant */
-    $tenant = Tenant::current();
-
-    /** @var TenantConfig $config */
-    $config = $tenant->config;
-
-    livewire(ManageAmazonSesSettings::class)
-        ->assertFormSet([
-            'fromName' => $config->mail->fromName,
-            'smtp_host' => $config->mail->mailers->smtp->host,
-            'smtp_port' => $config->mail->mailers->smtp->port,
-            'smtp_encryption' => $config->mail->mailers->smtp->encryption,
-            'smtp_username' => $config->mail->mailers->smtp->username,
-            'smtp_password' => $config->mail->mailers->smtp->password,
-            'smtp_timeout' => $config->mail->mailers->smtp->timeout,
-            'smtp_local_domain' => $config->mail->mailers->smtp->localDomain,
-        ]);
-});
-
-it('correctly edits the Amazon SES settings for the Tenant', function () {
-    $user = User::factory()->create();
-
-    asSuperAdmin($user);
-
-    /** @var Tenant $tenant */
-    $tenant = Tenant::current();
-
-    livewire(ManageAmazonSesSettings::class)
-        ->fillForm(
-            [
-                'fromName' => 'new-from-name',
-                'smtp_host' => 'new-smtp-host',
-                'smtp_port' => 123,
-                'smtp_encryption' => 'new-smtp-encryption',
-                'smtp_username' => 'new-smtp-username',
-                'smtp_password' => 'new-smtp-password',
-                'smtp_timeout' => 456,
-                'smtp_local_domain' => 'new-smtp-local-domain',
-            ]
-        )
-        ->call('save')
-        ->assertHasNoFormErrors();
-
-    $tenant->refresh();
-
-    expect($tenant->config->mail->fromName)->toBe('new-from-name')
-        ->and($tenant->config->mail->mailers->smtp->host)->toBe('new-smtp-host')
-        ->and($tenant->config->mail->mailers->smtp->port)->toBe(123)
-        ->and($tenant->config->mail->mailers->smtp->encryption)->toBe('new-smtp-encryption')
-        ->and($tenant->config->mail->mailers->smtp->username)->toBe('new-smtp-username')
-        ->and($tenant->config->mail->mailers->smtp->password)->toBe('new-smtp-password')
-        ->and($tenant->config->mail->mailers->smtp->timeout)->toBe(456)
-        ->and($tenant->config->mail->mailers->smtp->localDomain)->toBe('new-smtp-local-domain');
 });
