5c325faa44a583a7f4a38152170f0034f80eddde
Karl Murray <karl@karlm.dev>
2025-11-04 16:18:33 -0600

    [AIDAPP-734]: Feature Flag Cleanup: ParagraphTextColorFeature (#708)
    [AIDAPP-734]: Cleanup ParagaphTextColorFeature
    
    Co-authored-by: Karl Murray <karl.murray@canyongbs.com>

--- app-modules/integration-aws-ses-event-handling/database/migrations/2025_10_17_162227_add_paragraph_text_color_to_ses_settings.php
+++ app-modules/integration-aws-ses-event-handling/database/migrations/2025_10_17_162227_add_paragraph_text_color_to_ses_settings.php
@@ -34,28 +34,18 @@
 </COPYRIGHT>
 */
 
-use App\Features\ParagraphTextColorFeature;
-use Illuminate\Support\Facades\DB;
 use Spatie\LaravelSettings\Migrations\SettingsMigration;
 
 return new class () extends SettingsMigration {
     public function up(): void
     {
-        DB::transaction(function () {
-            if (! $this->migrator->exists('ses.paragraph_text_color')) {
-                $this->migrator->add('ses.paragraph_text_color', '#000000');
-            }
-
-            ParagraphTextColorFeature::activate();
-        });
+        if (! $this->migrator->exists('ses.paragraph_text_color')) {
+            $this->migrator->add('ses.paragraph_text_color', '#000000');
+        }
     }
 
     public function down(): void
     {
-        DB::transaction(function () {
-            ParagraphTextColorFeature::deactivate();
-
-            $this->migrator->deleteIfExists('ses.paragraph_text_color');
-        });
+        $this->migrator->deleteIfExists('ses.paragraph_text_color');
     }
 };
