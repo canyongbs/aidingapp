f49e103e589d16339d832524e82b2208a72bcf10
Dan Harrin <dan.harrin@canyongbs.com>
2025-11-12 13:56:00 +0000

    Update loading spinner SVG
    Signed-off-by: Dan Harrin <dan.harrin@canyongbs.com>

--- portals/knowledge-management/src/Pages/CreateServiceRequest.vue
+++ portals/knowledge-management/src/Pages/CreateServiceRequest.vue
@@ -312,26 +312,24 @@
                             class="pointer-events-auto flex items-center gap-3 px-4 py-2"
                         >
                             <svg
-                                class="animate-spin h-4 w-4 text-brand-600"
-                                xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="animate-spin h-4 w-4 text-brand-600"
                             >
-                                <circle
-                                    class="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    stroke-width="3"
-                                ></circle>
                                 <path
-                                    class="opacity-75"
+                                    clip-rule="evenodd"
+                                    d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                                    fill-rule="evenodd"
                                     fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                                    opacity="0.2"
+                                ></path>
+                                <path
+                                    d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
+                                    fill="currentColor"
                                 ></path>
                             </svg>
+
                             <span class="text-sm text-gray-700">Generating questions…</span>
                         </div>
                     </div>
