898e3ad6077a0a4e995ebb4a03e75c4e9af987e5
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-12-05 14:53:21 +0530

    create feature flag

---
+++ database/migrations/2025_12_05_091050_data_activate_pipeline_entry_feature_flag.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        //
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
