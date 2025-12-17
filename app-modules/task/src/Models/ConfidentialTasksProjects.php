0c8d615e8943f375d07946ab319f27cc24ba4458
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-12-17 20:41:28 +0530

    create task project

---
+++ app-modules/task/src/Models/ConfidentialTasksProjects.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AidingApp\Task\Models;
+
+use AidingApp\Project\Models\Project;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ConfidentialTasksProjects extends Pivot
+{
+
+    use HasUuids;
+
+    /**
+     * @return BelongsTo<Task, $this>
+     */
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    /**
+     * @return BelongsTo<Project, $this>
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
