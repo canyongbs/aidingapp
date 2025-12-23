f7b459e27ec6b581b3ee287789df835480a65869
jayushi-canyon <jayushi.jain@canyongbs.com>
2025-12-23 23:20:25 +0530

    Add conditions

--- app-modules/task/src/Models/Scopes/ConfidentialTaskScope.php
+++ app-modules/task/src/Models/Scopes/ConfidentialTaskScope.php
@@ -59,6 +59,24 @@ class ConfidentialTaskScope implements Scope
                         })
                         ->orWhereHas('confidentialAccessUsers', function (Builder $query) {
                             $query->where('user_id', auth()->id());
+                        })
+                        ->orWhereHas('project', function (Builder $query) {
+                            $query->whereHas('managerUsers', function (Builder $query) {
+                                    $query->where('user_id', auth()->id());
+                                })
+                                ->orWhereHas('managerTeams', function (Builder $query) {
+                                    $query->whereHas('users', function (Builder $query) {
+                                        $query->where('users.id', auth()->id());
+                                    });
+                                })
+                                ->orWhereHas('auditorUsers', function (Builder $query) {
+                                    $query->where('user_id', auth()->id());
+                                })
+                                ->orWhereHas('auditorTeams', function (Builder $query) {
+                                    $query->whereHas('users', function (Builder $query) {
+                                        $query->where('users.id', auth()->id());
+                                    });
+                                });
                         });
                 });
         });
