d43df4facb8862c00772851b7092779b26c6bbda
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2026-01-26 14:12:32 -0500

    feat: Add ChannelManager binding and create ChannelManagerTest for notification job validation
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

---
+++ app-modules/notification/tests/Tenant/Notifications/ChannelManagerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use AidingApp\Contact\Models\Contact;
+use AidingApp\Notification\Tests\Fixtures\TestEmailNotification;
+use Carbon\Carbon;
+use Illuminate\Notifications\SendQueuedNotifications;
+use Illuminate\Support\Facades\Queue;
+
+use function Pest\Laravel\freezeTime;
+
+it('modifies the SendQueuedNotifications job properly', function () {
+    Queue::fake();
+
+    $recipient = Contact::factory()->create();
+    $notification = new TestEmailNotification();
+
+    freezeTime(function () use ($recipient, $notification) {
+        $recipient->notify($notification);
+
+        Queue::assertPushed(SendQueuedNotifications::class, function ($job) use ($recipient, $notification) {
+            return $job->notification::class === $notification::class
+                && $job->notifiables->count() === 1
+                && $job->notifiables->first()->is($recipient)
+                && $job->channels === ['mail']
+                && $job->retryUntil() instanceof Carbon && $job->retryUntil()->equalTo(now()->addHours(2))
+                && $job->maxExceptions === 5;
+        });
+    });
+});
