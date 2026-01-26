1d5ddc6c8859fd452d73b72a67e55ad703425234
danharrin <41773797+danharrin@users.noreply.github.com>
2026-01-26 15:14:54 +0000

    Update code formatting and copyright headers

--- app-modules/service-management/src/Filament/Resources/ServiceRequestTypeResource/RelationManagers/ServiceRequestPrioritiesRelationManager.php
+++ app-modules/service-management/src/Filament/Resources/ServiceRequestTypeResource/RelationManagers/ServiceRequestPrioritiesRelationManager.php
@@ -45,7 +45,6 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
