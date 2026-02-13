6befc48ddadb6c342fc7a54824a8065e85a20926
Dan Harrin <git@danharrin.com>
2026-02-13 12:11:17 +0000

    Update AssistantBroadcastController.php

--- app-modules/ai/src/Http/Controllers/AssistantWidget/AssistantBroadcastController.php
+++ app-modules/ai/src/Http/Controllers/AssistantWidget/AssistantBroadcastController.php
@@ -60,6 +60,8 @@ class AssistantBroadcastController extends Controller
             throw new AccessDeniedHttpException();
         }
 
+        assert(method_exists($broadcaster, 'normalizeChannelName'));
+
         $normalizedName = $broadcaster->normalizeChannelName($channelName);
 
         if (! Str::startsWith($normalizedName, 'portal-assistant-thread-')) {
