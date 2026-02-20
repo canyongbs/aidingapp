46377ee5650a9c746308ce3eca133fa78acfd332
amit-canyon <amit.solanki@canyongbs.com>
2026-02-20 21:55:57 +0530

    Refactor project creation and permission checks to simplify code and improve readability

--- app-modules/project/src/Observers/ProjectObserver.php
+++ app-modules/project/src/Observers/ProjectObserver.php
@@ -42,9 +42,7 @@ class ProjectObserver
 {
     public function creating(Project $project): void
     {
-        if (is_null($project->createdBy)) {
-            $user = auth()->user();
-            $project->createdBy()->associate($user);
-        }
+        $user = auth()->user();
+        $project->createdBy()->associate($user);
     }
 }
