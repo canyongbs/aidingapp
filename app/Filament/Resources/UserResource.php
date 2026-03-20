1321862688ecdafb8ed7b1061fa34935091e3765
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2026-03-20 12:15:56 -0400

    Switch back to tap
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -70,9 +70,7 @@ class UserResource extends Resource
         /** @var Builder<User> $query */
         $query = parent::getEloquentQuery();
 
-        (new ConditionalAdminScope())($query);
-
-        return $query;
+        return $query->tap(new ConditionalAdminScope());
     }
 
     public static function getRelations(): array
