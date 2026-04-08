e019afcd0f917d563c9687c827d8db901e384c82
monali-canyon <263637457+monali-canyon@users.noreply.github.com>
2026-04-08 10:19:22 +0000

    Update code formatting and copyright headers

--- app-modules/service-management/database/migrations/2026_04_06_124120_add_default_issue_category_to_service_request_types_table.php
+++ app-modules/service-management/database/migrations/2026_04_06_124120_add_default_issue_category_to_service_request_types_table.php
@@ -37,8 +37,8 @@
 use AidingApp\ServiceManagement\Enums\ServiceRequestIssueCategory;
 use App\Features\ServiceRequestTypeDefaultIssueCategoryFeature;
 use Illuminate\Database\Migrations\Migration;
-use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class () extends Migration {
