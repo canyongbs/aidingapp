7c0d5aa30f2f66151bcb552d8ef16191d285b427
rebecca-canyon <198644076+rebecca-canyon@users.noreply.github.com>
2026-04-15 20:14:48 +0000

    Update code formatting and copyright headers

--- app-modules/knowledge-base/tests/Tenant/Filament/Actions/ChangeConcernStatusActionTest.php
+++ app-modules/knowledge-base/tests/Tenant/Filament/Actions/ChangeConcernStatusActionTest.php
@@ -1,6 +1,39 @@
 <?php
 
-use AidingApp\KnowledgeBase\Filament\Resources\KnowledgeBaseItems\Pages\ViewKnowledgeBaseItem;
+/*
+<COPYRIGHT>
+
+    Copyright © 2016-2026, Canyon GBS Inc. All rights reserved.
+
+    Aiding App® is licensed under the Elastic License 2.0. For more details,
+    see <https://github.com/canyongbs/aidingapp/blob/main/LICENSE.>
+
+    Notice:
+
+    - You may not provide the software to third parties as a hosted or managed
+      service, where the service provides users with access to any substantial set of
+      the features or functionality of the software.
+    - You may not move, change, disable, or circumvent the license key functionality
+      in the software, and you may not remove or obscure any functionality in the
+      software that is protected by the license key.
+    - You may not alter, remove, or obscure any licensing, copyright, or other notices
+      of the licensor in the software. Any use of the licensor’s trademarks is subject
+      to applicable law.
+    - Canyon GBS Inc. respects the intellectual property rights of others and expects the
+      same in return. Canyon GBS® and Aiding App® are registered trademarks of
+      Canyon GBS Inc., and we are committed to enforcing and protecting our trademarks
+      vigorously.
+    - The software solution, including services, infrastructure, and code, is offered as a
+      Software as a Service (SaaS) by Canyon GBS Inc.
+    - Use of this software implies agreement to the license terms and conditions as stated
+      in the Elastic License 2.0.
+
+    For more information or inquiries please visit our website at
+    <https://www.canyongbs.com> or contact us via email at legal@canyongbs.com.
+
+</COPYRIGHT>
+*/
+
 use AidingApp\KnowledgeBase\Filament\Widgets\KnowledgeBaseItemConcernsTable;
 use AidingApp\KnowledgeBase\Models\KnowledgeBaseItem;
 use AidingApp\KnowledgeBase\Models\KnowledgeBaseItemConcern;
@@ -12,22 +45,22 @@ use function Pest\Livewire\livewire;
 use function Tests\asSuperAdmin;
 
 it('is gated by proper access control', function () {
-  $user = User::factory()->create();
-  $user->givePermissionTo('knowledge_base_item.view-any');
+    $user = User::factory()->create();
+    $user->givePermissionTo('knowledge_base_item.view-any');
 
-  asSuperAdmin();
+    asSuperAdmin();
 
-  $knowledgeBaseItem = KnowledgeBaseItem::factory()->create();
-  KnowledgeBaseItemConcern::factory()->for($knowledgeBaseItem)->create();
+    $knowledgeBaseItem = KnowledgeBaseItem::factory()->create();
+    KnowledgeBaseItemConcern::factory()->for($knowledgeBaseItem)->create();
 
-  actingAs($user);
+    actingAs($user);
 
-  livewire(KnowledgeBaseItemConcernsTable::class, ['record' => $knowledgeBaseItem])
-    ->assertActionHidden([TestAction::make('changeConcernStatus')->table()]);
+    livewire(KnowledgeBaseItemConcernsTable::class, ['record' => $knowledgeBaseItem])
+        ->assertActionHidden([TestAction::make('changeConcernStatus')->table()]);
 
-  $user->givePermissionTo('knowledge_base_item.*.update');
+    $user->givePermissionTo('knowledge_base_item.*.update');
 
-  livewire(KnowledgeBaseItemConcernsTable::class, ['record' => $knowledgeBaseItem])
-    ->assertCountTableRecords(1)
-    ->assertActionVisible([TestAction::make('changeConcernStatus')->table()]);
+    livewire(KnowledgeBaseItemConcernsTable::class, ['record' => $knowledgeBaseItem])
+        ->assertCountTableRecords(1)
+        ->assertActionVisible([TestAction::make('changeConcernStatus')->table()]);
 });
