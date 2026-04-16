0e099c4130a8ebb5ec42aed425dcf92f6b9b5c94
nency-canyon <263891865+nency-canyon@users.noreply.github.com>
2026-04-16 13:53:01 +0000

    Update code formatting and copyright headers

--- app-modules/portal/src/Http/Controllers/KnowledgeManagementPortal/AssetManagementPortalController.php
+++ app-modules/portal/src/Http/Controllers/KnowledgeManagementPortal/AssetManagementPortalController.php
@@ -71,7 +71,7 @@ class AssetManagementPortalController extends Controller
             ->paginate(10);
 
         $items = $paginator->getCollection()->map(function ($checkOut) {
-            $isReturned = !empty($checkOut->asset_check_in_id);
+            $isReturned = ! empty($checkOut->asset_check_in_id);
 
             return [
                 'id' => $checkOut->getKey(),
