5eeeb9ab6ab0791fd649a93e4bf9219b1257e37c
nency-canyon <nency.bhadeshiya@canyongbs.com>
2026-04-28 16:15:08 +0530

    [AIDAPP-1076]: Modernize the details page for a service request  (#1049)
    * Modernize the service request detail view in the client portal
    
    * Remove dot from badges
    
    * Update code formatting and copyright headers
    
    * Add BaseInputError, BaseList, and BaseTextarea components; update ViewServiceRequest to utilize new components
    
    * Update code formatting and copyright headers
    
    * Adjust padding and whitespace handling for improved layout
    
    ---------
    
    Co-authored-by: nency-canyon <263891865+nency-canyon@users.noreply.github.com>

--- portals/knowledge-management/src/Components/Page.vue
+++ portals/knowledge-management/src/Components/Page.vue
@@ -66,8 +66,15 @@
                 <slot name="breadcrumbs" />
             </div>
 
-            <div class="ring-1 ring-black/5 shadow-xs xl:-mx-6 px-6 py-4 flex flex-col gap-y-6 xl:rounded bg-white">
+            <div
+                v-if="$slots.default"
+                class="ring-1 ring-black/5 shadow-xs xl:-mx-6 px-6 py-4 flex flex-col gap-y-6 xl:rounded bg-white"
+            >
                 <slot />
+            </div>
+
+            <div v-if="$slots.rawContent" class="px-6 xl:px-0 flex flex-col gap-y-4">
+                <slot name="rawContent" />
             </div>
         </div>
     </div>
