2c2e2412baba70e2389eb17a186985a5e15141c5
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-06-09 18:52:51 +0530

    remove feature flag changes

--- database/migrations/2026_05_12_254102_add_created_by_to_media_table.php
+++ database/migrations/2026_05_12_254102_add_created_by_to_media_table.php
@@ -36,25 +36,20 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
-    {
-        DB::transaction(function () {
-            Schema::table('media', function (Blueprint $table) {
-                $table->nullableUuidMorphs('created_by');
-            });
-        });
+    {        
+      Schema::table('media', function (Blueprint $table) {
+          $table->nullableUuidMorphs('created_by');
+      });
     }
 
     public function down(): void
-    {
-        DB::transaction(function () {
-            Schema::table('media', function (Blueprint $table) {
-                $table->dropMorphs('created_by');
-            });
-        });
+    {        
+      Schema::table('media', function (Blueprint $table) {
+          $table->dropMorphs('created_by');
+      });
     }
 };
