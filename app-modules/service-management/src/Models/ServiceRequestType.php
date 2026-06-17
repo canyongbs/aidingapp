f6cee000fc6bfcd3fb66c6248e9ceebd988c27d1
Mittal Solanki <mittal.parmar@canyongbs.com>
2026-06-17 19:37:24 +0530

    Remove redundant @property now covered by regenerated IDE helper

--- app-modules/service-management/src/Models/ServiceRequestType.php
+++ app-modules/service-management/src/Models/ServiceRequestType.php
@@ -63,8 +63,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
 /**
- * @property EmailAutomaticCreationContactCreateCondition $email_automatic_creation_contact_create_condition
- *
  * @mixin IdeHelperServiceRequestType
  */
 #[ObservedBy([ServiceRequestTypeObserver::class])]
