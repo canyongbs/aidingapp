91278dec26cea998120a70b0b4087b890e617535
amit-canyon <amit.solanki@canyongbs.com>
2026-06-24 20:52:25 +0530

    Remove unused User model import and update morphMap in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,7 +41,6 @@ use AidingApp\Notification\Enums\NotificationChannel;
 use App\Models\Media;
 use App\Models\SystemUser;
 use App\Models\Tenant;
-use App\Models\User;
 use App\Overrides\Filament\Actions\Imports\Jobs\ImportCsvOverride;
 use App\Overrides\Filament\Actions\Imports\Jobs\PrepareCsvExportOverride;
 use App\Overrides\Laravel\PermissionMigrationCreator;
@@ -102,7 +101,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Relation::morphMap([
-            'user' => User::class,
             'system_user' => SystemUser::class,
             'tenant' => Tenant::class,
             'email_settings_property' => EmailSettingsProperty::class,
