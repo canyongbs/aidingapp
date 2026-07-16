d470ae03f93e3c3244c4d00cc3faf2dcb7523a62
nency-canyon <nency.bhadeshiya@canyongbs.com>
2026-07-16 17:49:53 +0530

    fix: add missing docblock for IdeHelperProjectGuest

--- app-modules/project/src/Models/ProjectGuest.php
+++ app-modules/project/src/Models/ProjectGuest.php
@@ -39,6 +39,9 @@ namespace AidingApp\Project\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
 
+/**
+ * @mixin IdeHelperProjectGuest
+ */
 class ProjectGuest extends MorphPivot
 {
     use HasUuids;
