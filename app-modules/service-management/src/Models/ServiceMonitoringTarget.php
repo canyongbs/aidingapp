909714803f41da57e953c1d9dfbfe5ad7564652a
rebecca-canyon <rebecca.sheridan@canyongbs.com>
2026-08-12 15:34:06 -0400

    Remove redundant cast

--- app-modules/service-management/src/Models/ServiceMonitoringTarget.php
+++ app-modules/service-management/src/Models/ServiceMonitoringTarget.php
@@ -85,7 +85,6 @@ class ServiceMonitoringTarget extends BaseModel implements Auditable
     ];
 
     protected $casts = [
-        'frequency' => ServiceMonitoringFrequency::class,
         'is_reporting_active' => 'boolean',
         'report_frequency' => ServiceMonitoringReportFrequency::class,
         'is_reported_via_database' => 'boolean',
