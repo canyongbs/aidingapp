4893282168f301b8c27a0e29e777ccbe86053e70
rebecca-canyon <rebecca.sheridan@canyongbs.com>
2026-08-26 12:13:28 -0400

    Commit to trigger ci

--- app/Models/User.php
+++ app/Models/User.php
@@ -426,6 +426,8 @@ class User extends Authenticatable implements HasLocalePreference, FilamentUser,
 
     protected function serializeDate(DateTimeInterface $date): string
     {
-        return $date->format(config('project.datetime_format') ?? 'Y-m-d H:i:s');
+        return $date->format(
+            config('project.datetime_format') ?? 'Y-m-d H:i:s'
+        );
     }
 }
