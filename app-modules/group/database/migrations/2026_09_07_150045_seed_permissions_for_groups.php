3dbdb64fab18d519806c17c6fb2520cd305c0aca
amit-canyon <amit.solanki@canyongbs.com>
2026-09-08 17:31:12 +0530

    Refactor permission import path and enhance user validation in GroupObserver

--- app-modules/group/database/migrations/2026_09_07_150045_seed_permissions_for_groups.php
+++ app-modules/group/database/migrations/2026_09_07_150045_seed_permissions_for_groups.php
@@ -34,7 +34,7 @@
 </COPYRIGHT>
 */
 
-use CanyonGBS\Common\Database\Migrations\Concerns\CanModifyPermissions;
+use Database\Migrations\Concerns\CanModifyPermissions;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
