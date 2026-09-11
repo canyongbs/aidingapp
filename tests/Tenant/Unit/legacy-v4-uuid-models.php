12508be8af13916874a9b0938d1dae2cac46d50b
rebecca-canyon <rebecca.sheridan@canyongbs.com>
2026-09-11 16:48:46 -0400

    Address failing test

--- tests/Tenant/Unit/legacy-v4-uuid-models.php
+++ tests/Tenant/Unit/legacy-v4-uuid-models.php
@@ -142,6 +142,8 @@ use App\Models\FailedImportRow;
 use App\Models\HealthCheckResultHistoryItem;
 use App\Models\Import;
 use App\Models\LandlordSettingsProperty;
+use App\Models\NotificationSetting;
+use App\Models\NotificationSettingPivot;
 use App\Models\Pronouns;
 use App\Models\SettingsProperty;
 use App\Models\SystemUser;
@@ -258,6 +260,8 @@ return [
     HealthCheckResultHistoryItem::class,
     Import::class,
     LandlordSettingsProperty::class,
+    NotificationSetting::class, // TODO: Cleanup Task NotificationSettingsFeature - delete this line
+    NotificationSettingPivot::class, // TODO: Cleanup Task NotificationSettingsFeature - delete this line
     Pronouns::class,
     SettingsProperty::class,
     SystemUser::class,
