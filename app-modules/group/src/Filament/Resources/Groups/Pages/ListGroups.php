1d6715e90e9a459dbfbe08c6ae4c62b1625970d7
amit-canyon <amit.solanki@canyongbs.com>
2026-09-14 22:14:20 +0530

    Fix type hint for description closure in ListGroups table

--- app-modules/group/src/Filament/Resources/Groups/Pages/ListGroups.php
+++ app-modules/group/src/Filament/Resources/Groups/Pages/ListGroups.php
@@ -58,7 +58,7 @@ class ListGroups extends ListRecords
         return $table
             ->columns([
                 TextColumn::make('name')
-                    ->description(fn (Group $record): ?string => Str::limit($record->description, 40))
+                    ->description(fn (Group $record): string => Str::limit($record->description, 40))
                     ->tooltip(fn (Group $record): ?string => $record->description)
                     ->searchable()
                     ->sortable(),
